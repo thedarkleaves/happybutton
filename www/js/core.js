240ebaf7b306121d333fbbe8ec5a60cca5f77e6f
thedarkleaves <thedar.kleaves@zohomail.com.au>
2020-03-09 14:17:17 +1300

    add the missing fucking semicolons

--- www/js/core.js
+++ www/js/core.js
@@ -2,6 +2,31 @@ function printdebug(content) {
   $("#debug").append(content+"<br>");
 }
 
+function startLogin() {
+  printdebug("the login function has been called.");      
+  // start login
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithRedirect(provider);
+  firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -37,7 +62,7 @@ var app = {
       // Initialize Firebase
       firebase.initializeApp(firebaseConfig);
             
-      printdebug("intialising happy button")
+      printdebug("intialising happy button");
       // custom bits 
       $("#happybutton").on("tap click",function(){
         newcolour = "#" + Math.floor(Math.random()*1000000);
@@ -49,27 +74,4 @@ var app = {
     },
 };
 
-function startLogin() {
-  printdebug("the login function has been called.")      
-  // start login
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithRedirect(provider);
-  firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // ...
-    }
-    // The signed-in user info.
-    var user = result.user;
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-}
+
