d3484f8b0fbe711b27e17e58b35b00c2940b20e8
thedarkleaves <thedar.kleaves@zohomail.com.au>
2020-03-09 14:03:46 +1300

    try and get the auth going

--- www/js/core.js
+++ www/js/core.js
@@ -1,8 +1,31 @@
 function printdebug(content) {
-  $("#debug").html(content);
+  $("#debug").append(content+"<br>");
 }
 
-$("#debug").html("core.js is running");
+function startLogin() {
+  printdebug("the login function has been called.")      
+  // start login
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithRedirect(provider);
+  firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
 
 var app = {
     // Application Constructor
@@ -53,27 +76,3 @@ var app = {
     },
 };
 
-function startLogin() {
-        
-  // start login
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithRedirect(provider);
-  firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // ...
-    }
-    // The signed-in user info.
-    var user = result.user;
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-}
